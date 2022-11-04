d0c503bd98c399b4bb04a78fdc3a17ddd3f520d6
Yura Nytsko <nytsko.yura@gmail.com>
2022-11-04 02:23:50 +0200

    Fix data condition

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const fetchApi = async () => {
     let response = await fetch('https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu');
     let data = await response.json();
-    if (data.error == 'Too many requests, please try again later.') {
+    if (data.error !== undefined) {
       setTimeout(() => {
         document.location.reload()
       }, 2000)
